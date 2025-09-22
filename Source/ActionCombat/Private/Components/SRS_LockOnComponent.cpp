a90fedb30cd2ce59e316ef3eb4783d285d1498a5
WillTheWater <willthewater@protonmail.com>
2025-09-22 13:51:52 +0200

    Add lock-on targeting system to character component
    Implemented a lock-on system in SRS_LockOnComponent, including toggling lock-on state, tracking target actor, and updating control rotation during lock-on. Added references to owner character, controller, and movement component for proper input and movement handling. Exposed lock-on state and target actor accessors.

--- Source/ActionCombat/Private/Components/SRS_LockOnComponent.cpp
+++ Source/ActionCombat/Private/Components/SRS_LockOnComponent.cpp
@@ -3,14 +3,30 @@
 
 #include "Components/SRS_LockOnComponent.h"
 
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
 
 USRS_LockOnComponent::USRS_LockOnComponent()
 {
 	PrimaryComponentTick.bCanEverTick = true;
 }
 
+void USRS_LockOnComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (!bIsLockOnActive || !TargetActor.IsValid()) { return; }
+	FVector OwnerLocation = GetOwner()->GetActorLocation();
+	FVector TargetLocation = TargetActor->GetActorLocation();
+	FRotator TargetRotation = UKismetMathLibrary::FindLookAtRotation(OwnerLocation, TargetLocation);
+	OwnerController->SetControlRotation(TargetRotation);
+}
+
 void USRS_LockOnComponent::ToggleLockOn(float TraceRadius)
 {
+	bIsLockOnActive = !bIsLockOnActive;
 	FHitResult Hit;
 	FVector Start = GetOwner()->GetActorLocation();
 	FCollisionShape Shape = FCollisionShape::MakeSphere(TraceRadius);
@@ -26,14 +42,27 @@ void USRS_LockOnComponent::ToggleLockOn(float TraceRadius)
 		Shape,
 		Params
 	);
-	if (HasFoundTarget)
+	
+	if (bIsLockOnActive && HasFoundTarget && Hit.GetActor())
 	{
-		FString DebugMessage = FString::Printf(TEXT("Lock On Toggled: %s"), *Hit.GetActor()->GetName());
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, DebugMessage);
+		TargetActor = Hit.GetActor();
+		OwnerController->SetIgnoreLookInput(true);
+		OwnerMovementComponent->bOrientRotationToMovement = false;
+		OwnerMovementComponent->bUseControllerDesiredRotation = true;
+	}
+	else
+	{
+		TargetActor = nullptr;
+		OwnerController->SetIgnoreLookInput(false);
+		OwnerMovementComponent->bOrientRotationToMovement = true;
+		OwnerMovementComponent->bUseControllerDesiredRotation = false;
 	}
 }
 
 void USRS_LockOnComponent::BeginPlay()
 {
 	Super::BeginPlay();
+	OwnerCharacter = Cast<ACharacter>(GetOwner());
+	OwnerController = Cast<APlayerController>(OwnerCharacter->GetController());
+	OwnerMovementComponent = OwnerCharacter->GetCharacterMovement();
 }
--- Source/ActionCombat/Public/Components/SRS_LockOnComponent.h
+++ Source/ActionCombat/Public/Components/SRS_LockOnComponent.h
@@ -7,6 +7,8 @@
 #include "SRS_LockOnComponent.generated.h"
 
 
+class UCharacterMovementComponent;
+
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), BlueprintType, Blueprintable)
 class ACTIONCOMBAT_API USRS_LockOnComponent : public UActorComponent
 {
@@ -14,10 +16,28 @@ class ACTIONCOMBAT_API USRS_LockOnComponent : public UActorComponent
 
 public:
 	USRS_LockOnComponent();
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION(BlueprintCallable, Category = "Action Combat")
 	void ToggleLockOn(float TraceRadius = 800.f);
 
+	bool IsLockOnActive() const { return bIsLockOnActive; }
+	AActor* GetTargetActor() const { return TargetActor.Get(); }
+
 protected:
 	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY()
+	ACharacter* OwnerCharacter;
+
+	UPROPERTY()
+	APlayerController* OwnerController;
+
+	UPROPERTY()
+	UCharacterMovementComponent* OwnerMovementComponent;
+
+	bool bIsLockOnActive { false };
+
+	TWeakObjectPtr<AActor> TargetActor;
 };
